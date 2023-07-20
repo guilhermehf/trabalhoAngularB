3f9c4d72c976ff7c381f59a66c9beb9d78519900
Guilherme <guilhermehf23@gmail.com>
2023-07-20 07:41:16 -0300

    Primeiro Commit fazendo formulario estatico

---
+++ src/app/tabela-produtos/tabela-produtos.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tabela-produtos',
+  templateUrl: './tabela-produtos.component.html',
+  styleUrls: ['./tabela-produtos.component.css']
+})
+export class TabelaProdutosComponent {
+
+  listaProdutos = [
+    { id:1, nome: "Produto 1", preco:10},
+    { id:2, nome: "Produto 2", preco:20},
+    { id:3, nome: "Produto 3", preco:30},
+    { id:4, nome: "Produto 4", preco:40},
+    { id:5, nome: "Produto 5", preco:50}
+  ]
+
+}
