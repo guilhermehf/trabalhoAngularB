d2f2b404d94d100c397e02b60eb1ee145a768ce4
Guilherme <guilhermehf23@gmail.com>
2023-07-20 08:53:03 -0300

    Formularios de Produtos

---
+++ src/app/form-produtos/form-produtos.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-produtos',
+  templateUrl: './form-produtos.component.html',
+  styleUrls: ['./form-produtos.component.css']
+})
+export class FormProdutosComponent {
+  produto: any = {id:0, nome:'', preco: 0.0};
+
+  cadastrar(){
+    alert(this.produto.nome);
+  }
+
+}
--- src/app/tabela-produtos/tabela-produtos.component.ts
+++ src/app/tabela-produtos/tabela-produtos.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class TabelaProdutosComponent {
 
-  listaProdutos = [
+  listaProdutos: any[] = [
     { id:1, nome: "Produto 1", preco:10},
     { id:2, nome: "Produto 2", preco:20},
     { id:3, nome: "Produto 3", preco:30},
